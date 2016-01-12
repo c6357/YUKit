fc891a704e02d59bcdb64eb2bf1082a407fa7a16
BruceYu <c6357@outlook.com>
2016-01-12 14:44:01 +0800

    tag 1.0.4

--- YUKit/uikit/category/UIAlertView+YU.h
+++ YUKit/uikit/category/UIAlertView+YU.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <BlocksKit+UIKit.h>
+//#import <BlocksKit+UIKit.h>
 
 @interface UIAlertView (YU)
 
@@ -17,8 +17,8 @@
 
 +(UIAlertView*)ShowConfirmInfo:(NSString*)info delegate:(id)delegate;
 
-+ (void)alertView:(NSString *)title message:(NSString *)message cancelTitle:(NSString *)cancelTitle cancelBlock:(void(^)())cancelBlock;
-
-+ (void)alertView:(NSString *)title message:(NSString *)message submitTitle:(NSString *)submitTitle submitBlock:(void(^)())submitBlock cancelTitle:(NSString *)cancelTitle cancelBlock:(void(^)())cancelBlock;
+//+ (void)alertView:(NSString *)title message:(NSString *)message cancelTitle:(NSString *)cancelTitle cancelBlock:(void(^)())cancelBlock;
+//
+//+ (void)alertView:(NSString *)title message:(NSString *)message submitTitle:(NSString *)submitTitle submitBlock:(void(^)())submitBlock cancelTitle:(NSString *)cancelTitle cancelBlock:(void(^)())cancelBlock;
 
 @end
