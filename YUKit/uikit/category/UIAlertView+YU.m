2a8a25e6f82108264053a5304fe59c9154040c55
BruceYu <c6357@outlook.com>
2016-01-12 09:50:08 +0800

    tag 1.0.2
    tag 1.0.2 add 'BlocksKit', '~> 2.2.5'

--- YUKit/uikit/category/UIAlertView+YU.m
+++ YUKit/uikit/category/UIAlertView+YU.m
@@ -47,35 +47,35 @@
     return alert;
 }
 
-//+ (void)alertView:(NSString *)title message:(NSString *)message cancelTitle:(NSString *)cancelTitle cancelBlock:(void(^)())cancelBlock
-//{
-//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:cancelTitle otherButtonTitles:nil];
-//    [alert bk_setHandler:^{
-//        if (cancelBlock) {
-//            cancelBlock();
-//        }
-//    }forButtonAtIndex:0];
-//    
-//    [alert show];
-//}
-//
-//
-//+ (void)alertView:(NSString *)title message:(NSString *)message submitTitle:(NSString *)submitTitle submitBlock:(void(^)())submitBlock cancelTitle:(NSString *)cancelTitle cancelBlock:(void(^)())cancelBlock
-//{
-//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:cancelTitle otherButtonTitles:submitTitle, nil];
-//    [alert bk_setHandler:^{
-//        if (cancelBlock) {
-//            cancelBlock();
-//        }
-//    }forButtonAtIndex:0];
-//    
-//    [alert bk_setHandler:^{
-//        if (submitBlock) {
-//            submitBlock();
-//        }
-//    } forButtonAtIndex:1];
-//    [alert show];
-//}
-//
++ (void)alertView:(NSString *)title message:(NSString *)message cancelTitle:(NSString *)cancelTitle cancelBlock:(void(^)())cancelBlock
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:cancelTitle otherButtonTitles:nil];
+    [alert bk_setHandler:^{
+        if (cancelBlock) {
+            cancelBlock();
+        }
+    }forButtonAtIndex:0];
+    
+    [alert show];
+}
+
+
++ (void)alertView:(NSString *)title message:(NSString *)message submitTitle:(NSString *)submitTitle submitBlock:(void(^)())submitBlock cancelTitle:(NSString *)cancelTitle cancelBlock:(void(^)())cancelBlock
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:cancelTitle otherButtonTitles:submitTitle, nil];
+    [alert bk_setHandler:^{
+        if (cancelBlock) {
+            cancelBlock();
+        }
+    }forButtonAtIndex:0];
+    
+    [alert bk_setHandler:^{
+        if (submitBlock) {
+            submitBlock();
+        }
+    } forButtonAtIndex:1];
+    [alert show];
+}
+
 
 @end
