b51e8d4687280522a31ae8b2ba95270c3d04af8c
BruceYu <c6357@outlook.com>
2016-04-20 13:36:33 +0800

    add tag 1.1.1

--- YUKit/uikit/category/UITableView+YU.h
+++ YUKit/uikit/category/UITableView+YU.h
@@ -15,6 +15,8 @@ typedef NS_ENUM(NSInteger,YUAnimation) {
 
 @interface UITableView (YU)
 
+-(void)yuScrollViewDidScroll:(UIScrollView *)scrollView Animation:(BOOL)animation;
+
 - (CAAnimationGroup*)reloadDataAnimate:(YUAnimation)animation willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath duration:(CFTimeInterval)duration completion:(void(^)())completion;
 
 -(void)yuReloadData;
